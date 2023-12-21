6215a74f0856dc206bfdf7d4eb4e80c4f961768d
GorkaLiul <gorkaliul@gmail.com>
2023-12-21 23:07:00 +0100

    This works lfmao

--- MyTkinterApp/build/gui.py
+++ MyTkinterApp/build/gui.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # from tkinter import *
 # Explicit imports to satisfy Flake8
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
-
+from tkinter.filedialog import askdirectory
 import pytube
 from pytube import YouTube
 
@@ -19,29 +19,30 @@ ASSETS_PATH = OUTPUT_PATH / Path(r"/home/gorkaliul/Desktop/MyTkinterApp/build/as
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
-#~/Downloads
-def click():
-    global link
-    global output
-    output = entry_2.get()
+def fileDialogClick():
+    global output  # declare output
+    output = askdirectory()
+def downloadClick():
+    global link#declare video link
     link = entry_1.get()
+
 
     yt = pytube.YouTube(link)
     video = yt.streams.get_highest_resolution()
     video.download(output)
-    print('Video has been downloaded')
+    print('Done')
 
 window = Tk()
-window.title('L-VideoDownloader v1.0')
-window.geometry("526x211")
+window.title('l-video downloader v1.01')
+window.geometry("443x171")
 window.configure(bg = "#FFFFFF")
 
 
 canvas = Canvas(
     window,
     bg = "#FFFFFF",
-    height = 211,
-    width = 526,
+    height = 171,
+    width = 443,
     bd = 0,
     highlightthickness = 0,
     relief = "ridge"
@@ -51,24 +52,80 @@ canvas.place(x = 0, y = 0)
 image_image_1 = PhotoImage(
     file=relative_to_assets("image_1.png"))
 image_1 = canvas.create_image(
-    189.0,
-    96.0,
+    221.0,
+    85.0,
     image=image_image_1
 )
 
 image_image_2 = PhotoImage(
     file=relative_to_assets("image_2.png"))
 image_2 = canvas.create_image(
-    263.0,
-    20.0,
+    188.0,
+    106.0,
     image=image_image_2
+)
+
+image_image_3 = PhotoImage(
+    file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(
+    189.0,
+    55.0,
+    image=image_image_3
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=fileDialogClick,
+    relief="flat"
+)
+button_1.place(
+    x=26.0,
+    y=93.0,
+    width=27.0,
+    height=23.0
+)
+
+image_image_4 = PhotoImage(
+    file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(
+    37.00000648449037,
+    51.0,
+    image=image_image_4
+)
+
+image_image_5 = PhotoImage(
+    file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
+    371.0,
+    162.0,
+    image=image_image_5
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=downloadClick,
+    relief="flat"
+)
+button_2.place(
+    x=382.0,
+    y=37.0,
+    width=51.0,
+    height=52.0
 )
 
 entry_image_1 = PhotoImage(
     file=relative_to_assets("entry_1.png"))
 entry_bg_1 = canvas.create_image(
-    209.5,
-    96.5,
+    191.0,
+    57.0,
     image=entry_image_1
 )
 entry_1 = Entry(
@@ -78,81 +135,31 @@ entry_1 = Entry(
     highlightthickness=0
 )
 entry_1.place(
-    x=72.0,
-    y=79.0,
-    width=275.0,
-    height=33.0
-)
-
-canvas.create_text(
-    9.0,
-    8.0,
-    anchor="nw",
-    text="Video Downloader\n",
-    fill="#000000",
-    font=("Inter SemiBold", 12 * -1)
-)
-
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
-    highlightthickness=0,
-    command = click,
-    relief="flat"
-)
-button_1.place(
-    x=378.0,
-    y=79.0,
-    width=99.0,
-    height=101.0
-)
-
-canvas.create_text(
-    24.0,
-    89.0,
-    anchor="nw",
-    text="URL:",
-    fill="#373737",
-    font=("Inter SemiBold", 12 * -1)
-)
-
-image_image_3 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    189.0,
-    146.0,
-    image=image_image_3
+    x=53.0,
+    y=49.0,
+    width=276.0,
+    height=14.0
 )
 
 entry_image_2 = PhotoImage(
     file=relative_to_assets("entry_2.png"))
 entry_bg_2 = canvas.create_image(
-    219.0,
-    146.5,
+    196.0,
+    105.0,
     image=entry_image_2
 )
-entry_2 = Entry(text = "~/Downloads",
+entry_2 = Entry(
     bd=0,
     bg="#D9D9D9",
     fg="#000716",
     highlightthickness=0
-    )
-entry_2.place(
-    x=91.0,
-    y=129.0,
-    width=256.0,
-    height=33.0
 )
-
-canvas.create_text(
-    24.0,
-    137.0,
-    anchor="nw",
-    text="Output:",
-    fill="#373737",
-    font=("Inter SemiBold", 12 * -1)
+entry_2.place(
+    x=58.0,
+    y=97.0,
+    width=276.0,
+    height=14.0
 )
 window.resizable(False, False)
 window.mainloop()
+
